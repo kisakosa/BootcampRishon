3d8cfaf30add0874e6bd203bf8e224195b65d2fa
ron <ron.ochak@gmail.com>
2024-09-07 18:10:33 +0300

    simplified mongodb connection logic

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-const indexRouter = require('./routes/index');
-const apiRouterRoutes = require('./routes/api/v1/route');
+// MongoDB connection
+mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('DB connection is open'));
+db.set("useCreateIndex", true);
+console.log(process.env.MONGOURL);
 
-// init database
-require('./classes/mongo')(app);
-
-// routes
-app.use('/', indexRouter);
-app.use('/api/v1/routes', apiRouterRoutes);
+// Routes import
+app.use('/', require('./routes/index'));
+app.use('/api/v1/routes', require('./routes/api/v1/route'));
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
