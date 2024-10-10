4035f22a2a321926125a897007347ab071542ae9
Git-Shira <shirae447@gmail.com>
2024-10-10 20:01:15 +0300

    Added API request to fetch and display a route on the map

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', async (req, res) => {
     return res.render('index', { title: 'Home' });
 });
 
+router.get('/map', async (req, res) => {
+    return res.render('mapScreen', { title: 'Map' });
+});
+
 router.get('/login', async (req, res) => {
     return res.render('login', { title: 'login' });
 });
